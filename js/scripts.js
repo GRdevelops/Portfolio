b445855f1a668a26f006f2a0c0c25ef434ce010e
GRdevelops <gianniruggieri99@gmail.com>
2023-11-03 16:00:42 +0100

    parallax and new lines

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,7 @@ function loadWork() {
           imageUrl: "./images/1.webp",
           title: "PrimoPiano Luxury Accomodations",
           subtitle: "- B&B in Vieste, Italy.",
-          description: "A cutting-edge digital platform designed to revolutionize the way people interact with technology. Back in the beginning of the year, I downloaded a template from envato market.",
+          description: "This live luxury room renting website combines a sleek Envato template with custom-added pages and a seamless booking feature, providing a modern and user-friendly interface that significantly improves upon the previous WordPress-based website.",
           firstSkill: "HTML",
           secondSkill: "CSS"
       },
@@ -35,7 +35,7 @@ function loadWork() {
           imageUrl: "./images/2.webp",
           title: "Welcome to the blog",
           subtitle: "- Simple blog with javascript functionalities",
-          description: "A cutting-edge digital platform designed to revolutionize the way people interact with technology. Back in the beginning of the year, I downloaded a template from envato market.",
+          description: "A user-friendly blog with practical JavaScript features including a persistent pop-up that stays closed upon page reload and a handy article sorting functionality that categorizes posts for streamlined navigation and improved reader experience.",
           firstSkill: "Javascript",
           secondSkill: "CSS"
       },
@@ -43,8 +43,8 @@ function loadWork() {
         imageUrl: "./images/5.webp",
         title: "Quote machine",
         subtitle: "- API connected quote generator using React",
-        description: "A cutting-edge digital platform designed to revolutionize the way people interact with technology. Back in the beginning of the year, I downloaded a template from envato market.",
-        firstSkill: "Javascript",
+        description: "A React-based quote generator that fetches and displays random quotes from an API, providing users with an interactive and inspirational experience.",
+        firstSkill: "React",
         secondSkill: "CSS"
     }
   ];
@@ -65,4 +65,49 @@ function loadWork() {
 };
 
 document.addEventListener("DOMContentLoaded", loadWork);
+
+
+
+//Animate lines on scroll
+
+const triggerAnimation = () => {
+  const elements = document.querySelectorAll('.line');
+  const windowHeight = window.innerHeight;
+  
+  elements.forEach(element => {
+    const rect = element.getBoundingClientRect();
+    
+    if (rect.top <= windowHeight * 0.9) {
+      element.classList.add('animate');
+    }
+  });
+}
+
+window.addEventListener('scroll', triggerAnimation);
+
+
+
+const headings = document.querySelectorAll('.repeated-heading');
+
+const setTranslateX = () => {
+  const scrollValue = window.scrollY;
+  const maxScrollValue = document.body.scrollHeight - window.innerHeight;
+
+  // Convert vertical scroll to a horizontal percentage
+  const scrollPercentage = scrollValue / maxScrollValue;
+
+  // Assuming your content is 100% wider than the viewport
+  const maxTranslateX = window.innerWidth;
+  
+  headings.forEach(heading => {
+    const translateXValue = -(maxTranslateX - (maxTranslateX * scrollPercentage));
+    heading.style.transform = `translateX(${translateXValue}px)`;
+  });
+};
+
+window.addEventListener('scroll', () => {
+  requestAnimationFrame(setTranslateX);
+});
+
+
 
